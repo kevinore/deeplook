082fc9e1b1fbcafe388c5c57271d17af6a9171a3
kevinore <kevin.ore@hunty.com>
2026-04-30 22:21:36 -0500

    Solve logs issue

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)],
+)
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
